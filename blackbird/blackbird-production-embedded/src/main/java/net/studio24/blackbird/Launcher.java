7682c8f03d28d497c91f198a5934483f9fb7e780
Frederik Fischer <ferdi@studio-24.net>
2016-06-11 15:45:46 +0200

    #2: Added maven configuration to build a laucher.jar with embedded dependencies (including blackbird.war) and a Launcher class that starts an embedded jetty webserver

--- blackbird/blackbird-production-embedded/src/main/java/net/studio24/blackbird/Launcher.java
+++ blackbird/blackbird-production-embedded/src/main/java/net/studio24/blackbird/Launcher.java
@@ -1,5 +1,11 @@
 package net.studio24.blackbird;
 
+import java.net.URL;
+import java.security.ProtectionDomain;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
 /**
  * The launcher for embedded webserver.
  * 
@@ -12,9 +18,32 @@ public class Launcher {
 	 * 
 	 * @param args
 	 *            the launcher arguments
+	 * @throws Exception
+	 *             is thrown in case of any failure
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		Server server = new Server(8080);
 
+		org.eclipse.jetty.webapp.Configuration.ClassList classlist = org.eclipse.jetty.webapp.Configuration.ClassList
+		        .setServerDefault(server);
+		classlist.addAfter("org.eclipse.jetty.webapp.FragmentConfiguration",
+		        "org.eclipse.jetty.plus.webapp.EnvConfiguration",
+		        "org.eclipse.jetty.plus.webapp.PlusConfiguration");
+		classlist.addBefore("org.eclipse.jetty.webapp.JettyWebXmlConfiguration",
+		        "org.eclipse.jetty.annotations.AnnotationConfiguration");
+
+		ProtectionDomain domain = Launcher.class.getProtectionDomain();
+		URL location = domain.getCodeSource().getLocation();
+
+		// Create the WebApp
+		WebAppContext webapp = new WebAppContext();
+		webapp.setContextPath("/");
+		webapp.setWar(location.toExternalForm());
+		server.setHandler(webapp);
+
+		// Start server
+		server.start();
+		server.join();
 	}
 
 }
