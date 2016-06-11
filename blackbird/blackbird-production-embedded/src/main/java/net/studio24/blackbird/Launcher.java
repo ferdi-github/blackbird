82a41a19b839e41dd5044d1f066c1e6323353213
Frederik Fischer <ferdi@studio-24.net>
2016-06-11 12:17:51 +0200

    #2: Added project stub for production module embedded

---
+++ blackbird/blackbird-production-embedded/src/main/java/net/studio24/blackbird/Launcher.java
@@ -0,0 +1,20 @@
+package net.studio24.blackbird;
+
+/**
+ * The launcher for embedded webserver.
+ * 
+ * @author Frederik Fischer
+ */
+public class Launcher {
+
+	/**
+	 * Application entry method.
+	 * 
+	 * @param args
+	 *            the launcher arguments
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
