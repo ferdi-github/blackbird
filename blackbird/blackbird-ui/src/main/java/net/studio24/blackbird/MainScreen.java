fbd4cec23c3a8171058efc6316bdfb843eab1c71
Frederik Fischer <ferdi@studio-24.net>
2016-06-12 20:41:23 +0200

    #7: Setup navigation using some dummy views

--- blackbird/blackbird-ui/src/main/java/net/studio24/blackbird/MainScreen.java
+++ blackbird/blackbird-ui/src/main/java/net/studio24/blackbird/MainScreen.java
@@ -1,27 +1,54 @@
 package net.studio24.blackbird;
 
-import com.vaadin.ui.Alignment;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.themes.ValoTheme;
+import com.vaadin.navigator.Navigator;
+import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.server.FontAwesome;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.UI;
 
-public class MainScreen extends VerticalLayout {
+public class MainScreen extends HorizontalLayout {
 
-	public MainScreen() {
-		buildUI();
-	}
+    private Menu menu;
 
-	private void buildUI() {
-		setStyleName("main-screen");
+    public MainScreen() {
+        buildUI();
+    }
 
-		setMargin(true);
-		setSizeFull();
+    private void buildUI() {
+        setStyleName("main-screen");
 
-		Label label = new Label("Blackbird");
-		label.addStyleName(ValoTheme.LABEL_H1);
-		label.setSizeUndefined();
-		addComponent(label);
-		setComponentAlignment(label, Alignment.MIDDLE_CENTER);
-	}
+        CssLayout viewContainer = new CssLayout();
+        viewContainer.addStyleName("valo-content");
+        viewContainer.setSizeFull();
+
+        final Navigator navigator = new Navigator(UI.getCurrent(), viewContainer);
+        navigator.setErrorView(ErrorView.class);
+        navigator.addViewChangeListener(viewChangeListener);
+
+        menu = new Menu(navigator);
+        menu.addView(new SampleViewDefault(), SampleViewDefault.VIEW_NAME, "Default", FontAwesome.EDIT);
+        menu.addView(new SampleViewA(), SampleViewA.VIEW_NAME, SampleViewA.VIEW_NAME, FontAwesome.EDIT);
+        menu.addView(new SampleViewB(), SampleViewB.VIEW_NAME, SampleViewB.VIEW_NAME, FontAwesome.INFO_CIRCLE);
+
+        addComponent(menu);
+        addComponent(viewContainer);
+        setExpandRatio(viewContainer, 1);
+        setSizeFull();
+    }
+
+    ViewChangeListener viewChangeListener = new ViewChangeListener() {
+
+        @Override
+        public boolean beforeViewChange(ViewChangeEvent event) {
+            return true;
+        }
+
+        @Override
+        public void afterViewChange(ViewChangeEvent event) {
+            menu.setActiveView(event.getViewName());
+        }
+
+    };
 
 }
